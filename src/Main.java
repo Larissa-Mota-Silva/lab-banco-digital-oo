86f2b7b2d46a587d604a12ca02bad3fcf96836f9
Larissa Mota <106595249+Larissa-Mota-Silva@users.noreply.github.com>
2024-07-28 21:33:34 -0300

    Update Main.java
    Adicionamento na classe main, implementação de consulta de clientes

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,40 @@
-
 public class Main {
 
-	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
-		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+    public static void main(String[] args) {
+        Banco banco = new Banco();
+        
+        Cliente venilton = new Cliente();
+        venilton.setNome("Venilton");
+        
+        Cliente maria = new Cliente();
+        maria.setNome("Maria");
 
-		cc.depositar(100);
-		cc.transferir(100, poupanca);
-		
-		cc.imprimirExtrato();
-		poupanca.imprimirExtrato();
-	}
+        Conta ccVenilton = new ContaCorrente(venilton);
+        Conta poupancaVenilton = new ContaPoupanca(venilton);
+        
+        Conta ccMaria = new ContaCorrente(maria);
+        Conta poupancaMaria = new ContaPoupanca(maria);
 
+        banco.adicionarCliente(venilton);
+        banco.adicionarCliente(maria);
+
+        banco.adicionarConta(ccVenilton);
+        banco.adicionarConta(poupancaVenilton);
+        banco.adicionarConta(ccMaria);
+        banco.adicionarConta(poupancaMaria);
+
+        ccVenilton.depositar(100);
+        ccVenilton.transferir(50, poupancaVenilton);
+
+        ccMaria.depositar(200);
+        ccMaria.transferir(100, poupancaMaria);
+
+        banco.listarClientes();
+        banco.listarContas();
+
+        ccVenilton.imprimirExtrato();
+        poupancaVenilton.imprimirExtrato();
+        ccMaria.imprimirExtrato();
+        poupancaMaria.imprimirExtrato();
+    }
 }
